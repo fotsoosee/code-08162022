00aeb0f805f9e4d4c9b03d6263dc3fe9e5d7da59
oseefotso <fotsoosee@gmail.com>
2022-08-16 21:11:21 -0400

    ec2 update

--- ec2.tf
+++ ec2.tf
@@ -3,5 +3,13 @@ module "ec2" {
   ami_id        = var.ami_id
   instance_type = var.instance_type
   instance_name = var.instance_name
+  resource "aws_ebs_volume" "example" {
+    availability_zone = "us-west-2a"
+    size              = 40
+  
+    tags = {
+      Name = "HelloWorld"
+    }
+  }
 
 }
